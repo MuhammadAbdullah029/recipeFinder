51877eacd93ff88b37dbbc024863644e2d2bc66d
MuhammadAbdullah029 <abdullahgu77ar@gmail.com>
2024-08-05 05:39:47 -0600

    some changes

--- src/components/Slidebar.jsx
+++ src/components/Slidebar.jsx
@@ -20,7 +20,7 @@ const DesktopSlidebar = () => {
         <div className="flex flex-col gap-20 sticky top-10 left-0 ">
 
         <div className="w-full flex justify-center">
-            <img src="/logo.PNG" alt="logo"  className="h-[10vw] md:h-[5vw]" />
+            <img src="/image.PNG" alt="logo"  className="h-[10vw] md:h-[5vw]" />
         </div>
         <ul className="flex flex-col items-center md:items-start gap-8 ">
             <Link to={"/"} className="flex gap-2" >
